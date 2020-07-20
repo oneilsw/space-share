49d366fbba428cc97da1c8cf4ceb74578edad2bc
Oneil <oneilswilliams@gmail.com>
2020-07-20 14:35:28 -0400

    Nav bar

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Menu, Container, Dropdown } from 'semantic-ui-react'
+import { Menu, Container, Dropdown, Header } from 'semantic-ui-react'
 
 const NavBar = () => {
   return (
     <>
-      "All Hail the NavBar"
+      <Menu pointing secondary>
+          <Menu.Item>
+            <Header as='h2' floated='left' content="Space Share"/>
+          </Menu.Item>
+          <Menu.Item
+            name='Spaces'
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='Contact Us'
+            />
+            <Menu.Item
+              name='Log In'
+            />
+          </Menu.Menu>
+        </Menu>
     </>
   )
 }
